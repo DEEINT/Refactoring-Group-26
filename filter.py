4b49f9c1ca9b6be244821aa8dc335484dc95f580
Filippov Timofey <43012401+DEEINT@users.noreply.github.com>
2021-11-17 01:43:10 +0500

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,28 +1,37 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
+
+def Find_medS(arr, i, j, mosSize):
+    medS = 0
+    for n in range(i, i + mosSize):
+        for n1 in range(j, j + mosSize):
+            red = arr[n][n1][0]
+            green = arr[n][n1][1]
+            blue = arr[n][n1][2]
+            pixel = (int(red) + int(green) + int(green))/3
+            medS += pixel
+    medS = int(medS // 100)
+    return medS
+
+def black_white(arr, grayscale, height, inp, mosSize, width):
+    for i in range(0, height - 1, mosSize):
+        for j in range(0, width - 1, mosSize):
+            medS = Find_medS(arr, i, j, mosSize)
+            for n in range(i, i + mosSize):
+                for n1 in range(j, j + mosSize):
+                    arr[n][n1][0] = int(medS // grayscale) * grayscale
+                    arr[n][n1][1] = int(medS // grayscale) * grayscale
+                    arr[n][n1][2] = int(medS // grayscale) * grayscale
+    res = Image.fromarray(arr)
+    res.save(inp[1])
+
+print('Введите полное имя исходного изображения и результата.',
+      'Размер мозайки. Градацию серого. (разделитель - " ")')
+inp = input().split(' ')
+img = Image.open(inp[0])
+mosSize = int(inp[2])
+grayscale = int(inp[3])
 arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 11:
-    j = 0
-    while j < a1 - 11:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+height = len(arr)
+width = len(arr[1])
+black_white(arr, grayscale, height, inp, mosSize, width)
